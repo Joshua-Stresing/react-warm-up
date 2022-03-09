2a0bfff10ca02fccbc11528389d0be8f449dde7b
Joshua Stresing <pyrotorch69@yahoo.com>
2022-03-09 09:45:33 -0800

    made some edits and notes

--- src/components/ImageItem/ImageItem.js
+++ src/components/ImageItem/ImageItem.js
@@ -1,11 +1,21 @@
 import './ImageItem.css';
 
-export default function ImageItem(props) {
+// export default function ImageItem(props) {
+//   return (
+//     <div className="image-item">
+//       <h3>{props.name}</h3>
+//       <img alt={props.name} width="100" src={props.url} />
+//       <p>{props.type}</p>
+//     </div>
+//   );
+// }
+
+export default function ImageItem({ name, type, url }) {
   return (
     <div className="image-item">
-      <h3>{props.name}</h3>
-      <img alt={props.name} width="100" src={props.url} />
-      <p>{props.type}</p>
+      <h3>{name}</h3>
+      <img alt={name} width="100" src={url} />
+      <p>{type}</p>
     </div>
   );
 }
